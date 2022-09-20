14ca2901350bc8771a34785f789e0a75ca39c3ce
Francesco Boasso <f.boasso@reply.it>
2022-09-20 13:39:18 +0200

    Non so bene a cosa serva ancora

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,67 @@
-#!/usr/bin/env node
-
 // read in env settings
 require('dotenv').config();
 
-const yargs = require('yargs');
-
 const fetch = require('./fetch');
 const auth = require('./auth');
+const readline = require('readline');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express();
 
-const options = yargs
-    .usage('Usage: --op <operation_name>')
-    .option('op', { alias: 'operation', describe: 'operation name', type: 'string', demandOption: true })
-    .argv;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var exit =1;
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
 
 async function main() {
-    console.log(`You have selected: ${options.op}`);
+    while (exit> 0) {
+        const ans = await askQuestion("\nChe cosa si fa? ");
+        switch (ans) {
+            case 'getUsers':
+                try {
+                    // here we get an access token
+                    const authResponse = await auth.getToken(auth.tokenRequest);
 
-    switch (yargs.argv['op']) {
-        case 'getUsers':
+                    // call the web API with the access token
+                    const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
 
-            try {
-                // here we get an access token
+                    // display result
+                    console.log(users);
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case '43U4':
                 const authResponse = await auth.getToken(auth.tokenRequest);
-
-                // call the web API with the access token
-                const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
-
-                // display result
-                console.log(users);
-            } catch (error) {
-                console.log(error);
-            }
-
-            break;
-        default:
-            console.log('Select a Graph operation first');
-            break;
+                const site = await fetch.callApi(process.env.GRAPH_ENDPOINT + '/v1.0/sites/' + process.env.HOSTNAME + process.env.CLIENT43U4, authResponse.accessToken);
+                console.log(site);
+                break;
+            case 'q':
+                exit =-1;
+                console.log("\r\nBye!")
+                process.exit(exit);
+                break;
+            case 'cc':
+                console.clear();
+                break;
+            default:
+                console.log('Non hai selezionato nulla! \n');
+                break;
+        }
     }
 };
 
