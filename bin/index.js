c2cdabe5bb01924cb07b0732612cd0c95b764ac8
Francesco Boasso <f.boasso@reply.it>
2022-09-27 15:38:49 +0200

    Modificata API per renderla disponibile con react

--- bin/index.js
+++ bin/index.js
@@ -3,71 +3,25 @@ require('dotenv').config();
 
 const fetch = require('./fetch');
 const auth = require('./auth');
-const readline = require('readline');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express();
-const fs = require('fs');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var exit =1;
-
-function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }))
-}
-
-async function main() {
-    while (exit> 0) {
-        const ans = await askQuestion("\r\nChe cosa si fa? ");
-        switch (ans) {
-            case 'getUsers':
-                try {
-                    // here we get an access token
-                    const authResponse = await auth.getToken(auth.tokenRequest);
-
-                    // call the web API with the access token
-                    const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
-
-                    // display result
-                    console.log(users);
-                } catch (error) {
-                    console.log(error);
-                }
-                break;
-            case '43U4':
-                const authResponse = await auth.getToken(auth.tokenRequest);
-                const site = await fetch.callApi(process.env.GRAPH_ENDPOINT + '/v1.0/sites/' + process.env.HOSTNAME + process.env.CLIENT43U4, authResponse.accessToken);
-                fs.appendFile("43U4.json", JSON.stringify(site, undefined), function(err, data) {
-                    if (err) throw err;
-                    console.log("salvato!");
-                });
-                console.log(site);
-                break;
-            case 'q':
-                exit =-1;
-                console.log("\r\nBye!")
-                process.exit(exit);
-                break;
-            case 'cc':
-                console.clear();
-                break;
-            default:
-                console.log('Non hai selezionato nulla! \n');
-                break;
-        }
+app.get('/43U4', async (req, res)=>{
+    try {
+        const authResponse = await auth.getToken(auth.tokenRequest);
+        const site = await fetch.callApi(process.env.GRAPH_ENDPOINT + '/v1.0/sites/' + process.env.HOSTNAME + process.env.TCM43U4, authResponse.accessToken);
+        res.send(site);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
-};
+});
 
-main();
+app.listen('3001', ()=> {
+    console.log('Running');
+});
