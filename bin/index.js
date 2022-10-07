4fa8fe9a8d16ec983d4ad09cfe29f8c2234b82d6
Francesco Boasso <f.boasso@reply.it>
2022-10-07 09:50:13 +0200

    Modificata API per renderla disponibile con react

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const fetch = require('./fetch');
 const auth = require('./auth');
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
